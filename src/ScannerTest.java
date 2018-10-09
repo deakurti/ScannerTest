004433453f44a95dcb8d5c9bee25885807be663c
BT_1N3_32@bktech.local <BT_1N3_32@bktech.local>
2018-10-09 14:48:29 -0400

    Initial commit

---
+++ src/ScannerTest.java
@@ -0,0 +1,13 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+    public class ScannerTest {
+        public static void main(String[] args) {
+            Scanner input = new Scanner(System.in);
+            System.out.println("What is your name?");
+
+
+
+
+        }
+    }
